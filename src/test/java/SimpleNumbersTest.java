bb566d608c027049f3062b0bb4445751ac64d6ef
cherrydan <cherrydan.1978@gmail.com>
2022-08-14 13:46:52 +0300

    Make genSimples test, gives example List without mistake, test passed

--- src/test/java/SimpleNumbersTest.java
+++ src/test/java/SimpleNumbersTest.java
@@ -8,7 +8,7 @@ public class SimpleNumbersTest {
 
     @Test
     public void simpleNumbersTest() {
-        List<Integer> numbers = Arrays.asList(2, 3, 5, 6); //проверочный List с ошибкой
+        List<Integer> numbers = Arrays.asList(2, 3, 5, 7); //проверочный List с ошибкой
         Assertions.assertEquals(SimpleNumbers.genSimples(10), numbers);
     }
 
